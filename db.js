dd0a7b018653b0cfd93dd759cc718c728a719ef6
zixx-bit <eddmichira@gmail.com>
2023-01-19 23:25:20 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,10 @@ const Pool = require ("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "kth18822",
+  password: "zixx13060",
   host: "localhost",
   port: 5432,
   database: "todo"
 });
-
 
 module.exports = pool;
