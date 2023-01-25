21aea034b838cd060c034cb420063f80d20cc2b3
zixx-bit <eddmichira@gmail.com>
2023-01-25 11:57:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
+import React, { Fragment } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+// components
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+     <Fragment>
+     <div className="container">
+     <InputTodo />
+     <ListTodos/>
+     </div>
+     </Fragment>
+   );
 }
 
 export default App;
